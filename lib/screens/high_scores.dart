960f9410d915032c5773b5a6dbaab0fd7d925d80
doniyorbek7376 <doniyorbek6439@gmail.com>
2020-02-05 21:08:04 +0500

    Pre-last commit

---
+++ lib/screens/high_scores.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class HighScores extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Complete this screen
+     return Container();
+  }
+}
